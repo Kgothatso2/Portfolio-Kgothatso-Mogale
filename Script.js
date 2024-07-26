242c2f2db2ae1669542863f273b652222a195e00
Kgothatso2 <136069953+Kgothatso2@users.noreply.github.com>
2024-07-26 14:04:37 +0200

    Javascript updates

---
+++ Script.js
@@ -0,0 +1,9 @@
+// script.js
+document.getElementById("menu-icon").addEventListener("click", function () {
+  var navLinks = document.getElementById("nav-links");
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
+});
